7fdf0e3e1d0a16247d24e07d985688fac710d9e2
fernandatrindade135 <fernanda.trindade12@gmail.com>
2025-04-23 10:33:18 +0000

    inserindo as direçoes

--- novatoxadrez.c
+++ novatoxadrez.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    // Movimento na horizontal da Torre usando o loop "for" 
+    // Movimento na horizontal da Torre usando o loop "for"  5 casas para a direita 
     printf("Movimento da Torre:\n");
     for (int i = 0; i < 5; i++) {
         printf(" Direita\n");
     }
 
-    // Movimento diagonal do Bispo usando o loop "while"  
+    // Movimento diagonal do Bispo usando o loop "while"  5 casas para cima e direita 
     printf("\nMovimento do Bispo: \n");
     int j = 0;
     while (j < 5) {
@@ -15,7 +15,7 @@ int main() {
         j++;
     }
 
-    // Movimento da Rainha usando o loop "do-while"
+    // Movimento da Rainha usando o loop "do-while" 8 casas para a esquerda 
     printf("\nMovimento da Rainha:\n");
     int k = 0;
     do {
